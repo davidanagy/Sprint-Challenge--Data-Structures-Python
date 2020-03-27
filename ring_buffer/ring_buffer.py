86bb54cd9655c8333cd54d1bd89c7c2d64a589d1
davidanagy <davidanagy@users.noreply.github.com>
2020-03-27 10:01:29 -0700

    completed stretch goal in ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -54,10 +54,30 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # Initialize current at 0 because that'll be the
+        # first value that gets replaced.
+        self.current = 0
+        # Looking at the test file, it wants the storage
+        # to always have capacity values. So initaliaze
+        # with only "None"s.
+        self.storage = [None] * capacity
 
     def append(self, item):
-        pass
+        # Replace the item in location "self.current"
+        self.storage[self.current] = item
+        if self.current < self.capacity - 1:
+            # Add one to current if it's not at the end of the list
+            self.current += 1
+        else:
+            # If it is at the end of the list, set it back to 0.
+            self.current = 0
 
     def get(self):
-        pass
+        storage_print = []
+        for item in self.storage:
+            # Only return items that aren't "None."
+            if item:
+                storage_print.append(item)
+
+        return storage_print
